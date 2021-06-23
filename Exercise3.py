cde512457bde18b4c9690f260fd135ef24d51bc7
nncm-xb <you@example.com>
2021-06-23 20:57:29 +0200

    progress ex 3 very slow to run on each opening higher disk radii makes it worse

--- Exercise3.py
+++ Exercise3.py
@@ -1,28 +1,35 @@
 import cv2
-import numpy as np
-import math
 from matplotlib import pyplot as plt
 import skimage.morphology
+
+
+def granolumetry(img):
+
+    e_values = [10, 20, 25, 30]
+    # Successive opening operations
+    for e in e_values:
+        print("Disk radii = ", e)
+        # perform morphological opening with increasing structuring element,
+        # here a disk.
+        img = skimage.morphology.dilation(img, skimage.morphology.disk(e))
+
+        # Compute surface area
+        # compute the sum of all the pixels of the image after each opening or only the pixels affected by the operation ?
+        surface_area = sum(sum(img))
+        print("Surface area: ", surface_area)
+
+        plt.subplot(111), plt.imshow(img, cmap='gray')
+        plt.title('image smoothed'), plt.xticks([]), plt.yticks([])
+        plt.show()
+
+    return img
+
 
 # import both images in grayscale
 bubbles = cv2.imread("images/granulometry1-min.jpg", 0)
 balls = cv2.imread("images/granulometry2-min.jpg", 0)
 
-
-def granolumetry(img):
-
-    # Successive opening operations
-    granule = 0
-
-    # Compute surface area
-    return granule
-
-
+print("-----Granulometry of the bubbles picture-----")
 granule1 = granolumetry(bubbles)
+print("-----Granulometry of the balls picture-----")
 granule2 = granolumetry(balls)
-
-plt.subplot(121), plt.imshow(granule1, cmap='gray')
-plt.title('granulometry of bubbles image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(granule2, cmap='gray')
-plt.title('granulometry of balls image'), plt.xticks([]), plt.yticks([])
-plt.show()
